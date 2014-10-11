3596b645a432f120fe5e910b9a1b548176d44b92
eduh <eduh@xs4all.nl>
2014-10-11 08:47:31 +0200

    Update p2w.php

--- p2w.php
+++ p2w.php
@@ -74,9 +74,9 @@ function get_pivotx_entries()
 		p2w_format($row['body'],$row['title']);
 		echo '<item>'.N;
 			echo '<pubDate>'.date("D, d M Y H:i:s T", strtotime($row['publish_date'])).'</pubDate>'.N;
-			echo '<dc:creator>Peter van Cooten</dc:creator>'.N;
-			echo '<guid isPermaLink="false">http://www.ambientblog.net/wordpress/?p='.$row['uid'].'</guid>'.N;
-			echo '<description>Beschrijving hier</description>'.N;
+			echo '<dc:creator>YOUR NAME</dc:creator>'.N;
+			echo '<guid isPermaLink="false">http://www.yoursitehere.com//wordpress/?p='.$row['uid'].'</guid>'.N;
+			echo '<description>YOUR DESCRIPTION</description>'.N;
 			echo '<title><![CDATA['. htmlentities($row['title']).']]></title>'.N;
 			//echo '<content:encoded><![CDATA['. str_replace(array(']','['), '',$row['introduction']).str_replace(array(']','['), '',$row['body']).']]></content:encoded>'.N;
 			//echo '<excerpt:encoded><![CDATA['. str_replace(array(']','['), '',$row['introduction']).']]></excerpt:encoded>'.N;
